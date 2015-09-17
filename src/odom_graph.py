4d93265438d3611805a47b8da87445fb999a5a5c
jfbrasil <jbrasil@clearpathrobotics.com>
2015-09-17 15:46:49 -0400

    Update odom_graph.py
    repairs not mapping under indigo

--- src/odom_graph.py
+++ src/odom_graph.py
@@ -34,6 +34,8 @@ def listener():
 
 	rospy.init_node('odom_graph', anonymous=True)
 
+#     changed publish to read "husky_velocity_controller/cmd_vel"
+#     change repairs issues under indigo 
 	rospy.Subscriber("husky_velocity_controller/odom", Odometry, odomCB)
 
 	rospy.spin()
